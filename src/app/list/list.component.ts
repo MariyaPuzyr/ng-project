d436599d43bd4852c5a32a988149062daa9ef1ae
MariyaPuzyr <mariyapyzur@gmail.com>
2020-03-10 16:15:56 +0200

    lesson 52 - add input and output characters info

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  characters;
+  @Input() characters;
+  @Output() characterSide = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit(): void {
