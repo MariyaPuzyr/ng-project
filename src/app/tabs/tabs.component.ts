09cbba8db79bc142ebf40861925aca47911315fa
MariyaPuzyr <mariyapyzur@gmail.com>
2020-04-13 14:59:44 +0300

    lesson 87 - cleaning ts file

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,34 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 
-import {StarWarsService} from '../star-wars.service';
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
-  public characters = [];
-  public chosenList = 'all';
-  public swService: StarWarsService;
-
-  constructor(swService: StarWarsService) {
-    this.swService = swService;
-  }
-
+  constructor() {}
   ngOnInit(): void {
-  }
-
-  public onChoose(side) {
-    this.chosenList = side ;
-  }
-  public getCharacters() {
-    // if (this.chosenList === 'all') {
-    //   return this.characters.slice();
-    // }
-    // return this.characters.filter((char) => {
-    //   return char.side === this.chosenList;
-    // });
-    this.characters = this.swService.getCharacters(this.chosenList);
-    return this.characters;
   }
 }
